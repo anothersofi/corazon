f894fac6bc5754262497a672d4a2c8d2d24f1203
anothersofi <113196283+anothersofi@users.noreply.github.com>
2022-09-09 10:26:35 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 220)
+    music.setVolume(255)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1)
+})
